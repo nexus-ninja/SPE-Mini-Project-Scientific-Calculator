9c547090ad09f37f1c48eac1cc2a405259cc75ef
radhu20 <radhikaaggarwal2015@gmail.com>
2024-02-16 12:27:10 +0530

    Added basic structure of Calculator

---
+++ Calculator.py
@@ -0,0 +1,26 @@
+# calculator.py
+
+def square_root(x):
+    # Placeholder for square root functionality
+    pass
+
+def factorial(x):
+    # Placeholder for factorial functionality
+    pass
+
+def natural_log(x):
+    # Placeholder for natural logarithm functionality
+    pass
+
+def power(x, b):
+    # Placeholder for power functionality
+    pass
+
+def main():
+    print("Scientific Calculator")
+    # Here you can add a menu-driven approach to call the above functions
+    # For now, we will just print a message
+    print("Functionality to be implemented")
+
+if __name__ == "__main__":
+    main()
