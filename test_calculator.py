99d1b717b58478bd0b5fa418a2db490a35257670
Radhika Aggarwal <75164584+radhu20@users.noreply.github.com>
2024-03-22 15:37:25 +0530

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -4,7 +4,7 @@ import pytest
 import math
 
 def test_square_root():
-    assert square_root(4) == 2
+    assert square_root(25) == 5
     assert square_root(16) == 4
     with pytest.raises(ValueError):
         square_root(-1)  # Test for a negative number, should raise ValueError
